bb73a74dab0258eb4f257ede345437c2706068cf
Davi Silva <davisilvaphoto@gmail.com>
2025-06-12 11:26:29 -0300

    part1: exercise 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,32 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
 
-const Part = ({ part, exercisesNumber }) => (
-  <p>
-    {part} {exercisesNumber}
-  </p>
-)
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>
+      {props.part} {props.exercisesNumber}
+    </p>
+  )
+}
 
-const Content = ({ parts }) => (
-  <>
-    <Part part={parts.part1} exercisesNumber={parts.exercises1} />
-    <Part part={parts.part2} exercisesNumber={parts.exercises2} />
-    <Part part={parts.part3} exercisesNumber={parts.exercises3} />
-  </>
-)
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <Part part={props.parts.part1} exercisesNumber={props.parts.exercises1} />
+      <Part part={props.parts.part2} exercisesNumber={props.parts.exercises2} />
+      <Part part={props.parts.part3} exercisesNumber={props.parts.exercises3} />
+    </>
+  )
+}
 
-const Total = ({ exercises1, exercises2, exercises3 }) => (
-  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-)
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  console.log({ exercises1, exercises2, exercises3 })
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+}
 
 export const App = () => {
   const course = 'Desenvolvimento de aplicação Half Stack'
