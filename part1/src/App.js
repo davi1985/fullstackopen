a26338b32d3cd6d17491de4385cf5cb6bdeb1098
Davi Silva <davisilvaphoto@gmail.com>
2025-06-12 12:11:17 -0300

    part1: exercise 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -30,24 +30,29 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 
 export const App = () => {
   const course = 'Desenvolvimento de aplicação Half Stack'
-  const part1 = 'Fundamentos da biblioteca React'
-  const exercises1 = 10
-  const part2 = 'Usando props para passar dados'
-  const exercises2 = 7
-  const part3 = 'Estado do componente'
-  const exercises3 = 14
+
+  const part1 = {
+    name: 'Fundamentos da biblioteca React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Usando props para passar dados',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'Estado de um componente',
+    exercises: 14,
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={{ part1, part2, part3, exercises1, exercises2, exercises3 }}
-      />
+      <Content parts={{ part1, part2, part3 }} />
 
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </div>
   )
